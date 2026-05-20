35b00fbbaf34ff9cd9665d287a827c611d831605
Aayush219 <abdbhartiya.1999@gmail.com>
2026-05-21 00:21:33 +1000

    Add Interaction using Gradio

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ from datetime import datetime
 from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 
+import gradio as gr 
+
+
 def get_date():
     """ Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
+
+
 llm = ChatOllama(model = "qwen2.5:3b")
 
 system_prompt = """ You are a helpful assistant.
+Answer all user's queries.
 Use the get_date tool if the user is asking about today's date.
 """
 
 agent = create_agent(model = llm, tools = [get_date], system_prompt = system_prompt)
 
-user_query = input("Enter a query:")
 
-response = agent.invoke({"messages":[{"role": "user", "content": user_query}]})
+def chat(message, history):
+    response = agent.invoke({"messages":[{"role": "user", "content": message}]})
+    last_response = response['messages'][-1].content
+    return last_response
 
-print(response['messages'][-1].content)
+#print(response['messages'][-1].content)
+
+with gr.Blocks() as demo:
+    gr.Markdown("#AI Chatbot")
+    gr.ChatInterface(fn=chat)
+
+demo.launch()
